74038da6eae4109f78b250494032d5b7d37ae8c9
Emyzinha <101672818+Emyzinha@users.noreply.github.com>
2022-04-11 22:13:12 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+
 
 namespace CalculadoraIMC
 {
@@ -16,56 +17,7 @@ namespace CalculadoraIMC
             Console.WriteLine("qual sua peso em kg?");
             float peso = Convert.ToSingle(Console.ReadLine());
 
-            double imc = Math.Round(peso / (altura * altura), 2);
-
-            Individuo pessoa = new Individuo( altura, peso);
-
-
-            Console.WriteLine("{0} sua altura é {1} e seu peso {2}" , nome, altura, peso);
-            Console.WriteLine("seu IMC {0}", imc);
-          
-
-           if (imc < 17.00 )
-            {
-             Console.WriteLine("Muito abaixo do peso");
-            }
-            else if (imc >= 17.00 && imc < 18.50)
-            {
-             Console.WriteLine("Abaixo do peso {0}", imc);
-            }
-            else if (imc >= 18.50 && imc < 25.00)
-            {
-             Console.WriteLine("Peso normal {0}", imc);
-            }
-            else if (imc >= 25.00 && imc < 30.00 )
-            {
-             Console.WriteLine("Acima do peso {0}", imc);
-            }
-            else if (imc >= 30.00 && imc < 35.00  )
-            {
-             Console.WriteLine("Obesidade {0}", imc);
-            }
-            else if (imc >= 35.00 && imc < 40.00 )
-            {
-             Console.WriteLine("Obesidade II (severa) {0}", imc);
-            }
-            else 
-            {
-             Console.WriteLine("Obesidade III (mórbida) {0}", imc);
-            }
-
-            double engordar = (18.50 - imc);
-
-            if (imc < 18.50)
-            {
-              Console.WriteLine( "você precisa engordar {0}", engordar);  
-            }
-
-
-            // 18.50    25.00
-
-        
-            
+            Individuo pessoa = new Individuo( altura, peso, nome, imc);
 
         }
     }
